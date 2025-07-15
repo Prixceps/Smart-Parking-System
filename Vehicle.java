683407b70969233fff3b9a7f858535c2f14cdce4
Prince Raj <122321907+Prixceps@users.noreply.github.com>
2025-07-15 12:49:02 +0530

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Vehicle {
+    String number;
+    Date entryTime;
+
+    public Vehicle(String number, Date entryTime) {
+        this.number = number;
+        this.entryTime = entryTime;
+    }
+}
